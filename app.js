2899309f43687a2f36f3f6b69d29730f912e82d3
Zack <bostianz@gmail.com>
2016-01-18 10:40:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 window.onload = function() {
     var i = 0,
-    slideText = document.querySelector('#text'),
-    rightArrow = document.querySelector('#rightArrow');
-    leftArrow = document.querySelector('#leftArrow');
-    //turn this into an array of objects
-    //objects hold slide content and image { img: src, slide: content }
-    slides = ['Sign up on Digital ocean and create an ubuntu droplet.',
-    'Go to www.namecheape.com and purchase a domain name of your choice.',
-    'Point your purchased domain name to the three digital ocean nameservers: ns1.digitalocean.com(ns2,ns3)',
-    'Go to your created digital ocean droplet and create an A name document',
-    'Set the host name of your A document to "www" and the IP of your droplet',
-    'Download an SFTP client such as Filezilla',
-    'Log into your droplet server with username: root and your password',
-    'log into your server directory via filezilla',
-    'find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com)'
-  ];
+        slideText = document.querySelector('#text'),
+        rightArrow = document.querySelector('#rightArrow');
+        leftArrow = document.querySelector('#leftArrow');
+
+        //turn this into an array of objects
+        //objects hold slide content and image { img: src, slide: content }
+        slides = [
+        'Sign up on Digital ocean and create an ubuntu droplet.',
+        'Go to www.namecheape.com and purchase a domain name of your choice.',
+        'Point your purchased domain name to the three digital ocean nameservers: ns1.digitalocean.com(ns2,ns3).',
+        'Go to your created digital ocean droplet and create an A name document.',
+        'Set the host name of your A document to "www" and the IP of your droplet.',
+        'Download an SFTP client such as Filezilla.',
+        'Log into your droplet server with username: root and your password.',
+        'Log into your server directory via filezilla.',
+        'Find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com).'
+      ];
 
   //create a function that adds the content based on the array index position
   function showSlide(slide) {
@@ -33,12 +35,12 @@ window.onload = function() {
   //function that goes back through slides
   //better way of writing this?
   function clickToPreviousSlide() {
-    if (i >= slides.length) {
+    if (i <= slides.length) {
     i--;
     showSlide(slides[i]);
     console.log(i);
-    }
   }
+}
   //attach event listeners to the document and create a conditional.
   document.querySelector('html').addEventListener('click', function(event) {
     var el = event.target;
@@ -50,9 +52,9 @@ window.onload = function() {
     } else if (el === leftArrow) {
       console.log('left arrow');
       clickToPreviousSlide();
-    } else {
-      //do nothing
-      event.stopPropagation();
-    }
+    // } else {
+    //   //do nothing
+    //   event.stopPropagation();
+     }
   }, false);
 };
