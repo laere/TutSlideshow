cfa20162d0b39b2b7153db72a8c6ac0fd6a7c281
Zack Bostian <bostianz@gmail.com>
2016-01-18 09:55:29 -0500

    Slides can go forward and backward

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var i = 0,
+    var i = 0,
     slideText = document.querySelector('#text'),
     rightArrow = document.querySelector('#rightArrow');
     leftArrow = document.querySelector('#leftArrow');
@@ -16,35 +16,43 @@ window.onload = function() {
     'find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com)'
   ];
 
-//create a function that adds the content based on the array index position
-function showSlide(slide) {
-  slideText.innerHTML = slide;
-}
-//create a function that goes forward through the array content in intervals
-//bind the function to an event listener conditional
-function clickToNextSlide() {
-  if (i <= slides.length) {
-    i++;
+  //create a function that adds the content based on the array index position
+  function showSlide(slide) {
+    slideText.innerHTML = slide;
+  }
+  //create a function that goes forward through the array content in intervals
+  //bind the function to an event listener conditional
+  //better way of writing this?
+  function clickToNextSlide() {
+    if (i <= slides.length) {
+      i++;
+      showSlide(slides[i]);
+      console.log(i);
+    }
+  }
+  //function that goes back through slides
+  //better way of writing this?
+  function clickToPreviousSlide() {
+    if (i >= slides.length) {
+    i--;
     showSlide(slides[i]);
+    console.log(i);
+    }
   }
-}
-function clickToPreviousSlide() {
-
-}
-//attach event listeners to the document and create a conditional.
-document.querySelector('html').addEventListener('click', function(event) {
-  var el = event.target;
-  //if right arrow is clicked
-  if (el === rightArrow) {
-    console.log('right arrow!');
-    clickToNextSlide();
-  //else if left arrow is clicked
-  } else if (el === leftArrow) {
-    console.log('left arrow');
-  } else {
-    //do nothing
-    event.stopPropagation();
-  }
-}, false);
-
+  //attach event listeners to the document and create a conditional.
+  document.querySelector('html').addEventListener('click', function(event) {
+    var el = event.target;
+    //if right arrow is clicked
+    if (el === rightArrow) {
+      console.log('right arrow!');
+      clickToNextSlide();
+      //else if left arrow is clicked
+    } else if (el === leftArrow) {
+      console.log('left arrow');
+      clickToPreviousSlide();
+    } else {
+      //do nothing
+      event.stopPropagation();
+    }
+  }, false);
 };
