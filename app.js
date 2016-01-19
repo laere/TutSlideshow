88152d3e2b945a2ce9ba319156878a28e01617b4
Zack Bostian <bostianz@gmail.com>
2016-01-19 09:23:47 -0500

    Got image sliding working

--- app.js
+++ app.js
@@ -1,54 +1,83 @@
 window.onload = function() {
+  //function calls (hoisting)
+
+
   var currentSlidePosition = 0;
+  var currentImagePosition = 0;
   var slideText = document.querySelector('#text');
+  var slideImage = document.querySelector('#image');
   var rightArrow = document.querySelector('#rightArrow');
   var leftArrow = document.querySelector('#leftArrow');
 
-  //turn this into an array of objects
-  //objects hold slide content and image { img: src, slide: content }
-  slides = [
-    'Sign up on Digital ocean and create an ubuntu droplet.',
-    'Go to www.namecheap.com and purchase a domain name of your choice.',
-    'Point your purchased domain name to the three digital ocean nameservers: ns1.digitalocean.com(ns2,ns3).',
-    'Go to your created digital ocean droplet and create an A name document.',
-    'Set the host name of your A document to "www" and the IP of your droplet.',
-    'Download an SFTP client such as Filezilla.',
-    'Log into your droplet server with username: root and your password.',
-    'Log into your server directory via filezilla.',
-    'Find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com).'
+  var slides = [
+    '1. Sign up on Digital ocean and create an ubuntu droplet.',
+    '2. Go to www.namecheap.com and purchase a domain name of your choice.',
+    '3. Point your purchased domain name to the three digital ocean nameservers: ns1.digitalocean.com(ns2,ns3).',
+    '4. Go to your created digital ocean droplet and create an A name document.',
+    '5. Set the host name of your A document to "www" and the IP of your droplet.',
+    '6. Download an SFTP client such as Filezilla.',
+    '7. Log into your droplet server with username: root and your password.',
+    '8. Log into your server directory via filezilla.',
+    '9. Find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com).'
   ];
 
-  //create a function that adds the content based on the array index position
-  function showSlide(slide) {
-    slideText.innerHTML = slide;
-  }
+  var images = [
+    'http://lorempixel.com/image_output/abstract-q-c-600-250-10.jpg',
+    'http://lorempixel.com/image_output/animals-q-c-600-250-10.jpg',
+    'http://lorempixel.com/image_output/business-q-c-600-250-3.jpg',
+    'http://lorempixel.com/image_output/cats-q-c-600-250-5.jpg',
+    'http://lorempixel.com/image_output/city-q-c-600-250-5.jpg',
+    'http://lorempixel.com/image_output/food-q-c-600-250-7.jpg',
+    'http://lorempixel.com/image_output/nightlife-q-c-600-250-1.jpg',
+    'http://lorempixel.com/image_output/fashion-q-c-600-250-5.jpg',
+    'http://lorempixel.com/image_output/people-q-c-600-250-7.jpg'
+  ];
+  addFirstSlide();
+  addFirstImage();
   //add first slide
   function addFirstSlide() {
     slideText.innerHTML = slides[0];
   }
-  addFirstSlide();
+  //create a function that adds the content based on the array index position
+  function showSlide(slide) {
+    slideText.innerHTML = slide;
+  }
+  function addFirstImage() {
+    slideImage.setAttribute('src', images[0]);
+  }
+  function showImage(image) {
+    slideImage.setAttribute('src', image);
+  }
   //create a function that goes forward through the array content in intervals
   //bind the function to an event listener conditional
   //better way of writing this?
   function clickToNextSlide() {
-    console.log(slides.length);
-    if (currentSlidePosition < slides.length - 1) {
+    console.log('Slide length: ' + slides.length);
+    if (currentSlidePosition < slides.length - 1 && currentImagePosition < images.length - 1) {
       currentSlidePosition++;
+      currentImagePosition++;
       showSlide(slides[currentSlidePosition]);
-      console.log(currentSlidePosition);
+      showImage(images[currentImagePosition]);
+      console.log('Slide position: ' + currentSlidePosition);
+      console.log('Image position: ' + currentImagePosition);
     }
   }
   //function that goes back through slides
   //better way of writing this?
   function clickToPreviousSlide() {
-    console.log(slides.length);
-    if (currentSlidePosition <= slides.length) {
+    console.log('Slide length: ' + slides.length);
+    if (currentSlidePosition <= slides.length && currentImagePosition <= images.length) {
       currentSlidePosition--;
-      if (currentSlidePosition <= -1) {
+      currentImagePosition--;
+      console.log('Slide position: ' + currentSlidePosition);
+      console.log('Image position: ' + currentImagePosition);
+      if (currentSlidePosition <= -1 && currentImagePosition <= -1) {
         return;
       }
       showSlide(slides[currentSlidePosition]);
-      console.log(currentSlidePosition);
+      showImage(images[currentImagePosition]);
+      console.log('Slide position: ' + currentSlidePosition);
+      console.log('Image position: ' + currentImagePosition);
     }
   }
   //attach event listeners to the document and create a conditional.
@@ -62,9 +91,9 @@ window.onload = function() {
     } else if (el === leftArrow) {
       console.log('left arrow');
       clickToPreviousSlide();
-      // } else {
-      //   //do nothing
-      //   event.stopPropagation();
+    } else {
+      // do nothing
+      event.stopPropagation();
     }
   }, false);
 };
---
+++ slides.js
@@ -0,0 +1,14 @@
+
+window.onload = function() {
+  slides = [
+    '1. Sign up on Digital ocean and create an ubuntu droplet.',
+    '2. Go to www.namecheap.com and purchase a domain name of your choice.',
+    '3. Point your purchased domain name to the three digital ocean nameservers: ns1.digitalocean.com(ns2,ns3).',
+    '4. Go to your created digital ocean droplet and create an A name document.',
+    '5. Set the host name of your A document to "www" and the IP of your droplet.',
+    '6. Download an SFTP client such as Filezilla.',
+    '7. Log into your droplet server with username: root and your password.',
+    '8. Log into your server directory via filezilla.',
+    '9. Find the /var/www/ folder and create a folder with your purchased domain name (mydomain.com).'
+  ];
+};
